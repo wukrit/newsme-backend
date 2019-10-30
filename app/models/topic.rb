197862131e0d900500be85a1ce146131d04d1a86
wukrit <sukritwalia@gmail.com>
2019-10-30 15:34:37 -0400

    added has_many and belongs_to relationships to models

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,6 @@
 class Topic < ApplicationRecord
+  has_many :subscriptions
+  has_many :articles
+  has_many :users, through: :subscriptions
+  has_many :sources, through: :articles
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :filters
+  has_many :subscriptions
+  has_many :topics, through: :subscriptions
+  has_many :sources, through: :filters
 end
