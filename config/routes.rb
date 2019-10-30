5f869bede2a946d4ef5e01244e8194f01a519c64
wukrit <sukritwalia@gmail.com>
2019-10-30 15:26:22 -0400

    added resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :subscriptions
-  resources :filters
-  resources :sources
-  resources :articles
-  resources :topics
-  resources :users
+  resources :subscriptions, only: [:index, :show, :create, :destroy]
+  resources :filters, only: [:index, :show, :create, :destroy]
+  resources :sources, only: [:index, :show]
+  resources :articles, only: [:index, :show]
+  resources :topics, only: [:index, :show]
+  resources :users, only: [:show, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
