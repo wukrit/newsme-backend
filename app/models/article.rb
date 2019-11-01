d3b5fdcfab36ff90a30794061fbd092d6bedafe2
wukrit <sukritwalia@gmail.com>
2019-11-01 11:16:59 -0400

    added headline helper

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,15 @@
 class Article < ApplicationRecord
     belongs_to :topic
     belongs_to :news_source
+
+    require 'news-api'
+
+  def self.get_top_headlines(category)
+    newsapi = News.new(ENV['news_api_key'])
+    top_headlines =
+      newsapi.get_top_headlines(
+        category: category, language: 'en', country: 'us'
+      )
+  end
+
 end
