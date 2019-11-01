dfe8af1f10fd18ea057acc36441c1d638274b8e6
wukrit <sukritwalia@gmail.com>
2019-11-01 15:44:11 -0400

    updated routes, added jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :news_sources
-  resources :subscriptions
-  resources :filters
-  resources :articles
-  resources :topics
-  resources :users
+  resources :news_sources, only: [:index, :show]
+  resources :subscriptions, only: [:index, :create, :destroy]
+  resources :filters, only: [:index, :create, :destroy]
+  resources :articles, only: [:index, :show]
+  resources :topics, only: [:index, :show]
+  resources :users, only: [:show, :create, :update, :destroy]
+
+  post '/login', to: 'login#login'
+  get '/login', to: 'login#persist'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
