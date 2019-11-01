b5eeeb0530f85897338b2242a1c3a21e4b1f89e9
wukrit <sukritwalia@gmail.com>
2019-11-01 15:48:32 -0400

    added secure password created login controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
     has_many :topics, through: :subscriptions
     has_many :filters, dependent: :destroy
     has_many :news_sources, through: :filters
+
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 end
