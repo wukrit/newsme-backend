f1e9f81c31a1bd4a3eb59b5f51bf23de6b723d85
wukrit <sukritwalia@gmail.com>
2019-11-03 17:23:16 -0500

    added serve route and method for topics controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   resources :filters, only: [:index, :create, :destroy]
   resources :articles, only: [:index, :show]
   resources :topics, only: [:index, :show]
-  resources :users, only: [:show, :create, :update, :destroy]
+  resources :users, only: [:create, :update, :destroy]
 
   post '/login', to: 'login#login'
   get '/login', to: 'login#persist'
+
+  get '/topics/serve', to: 'topics#serve'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
