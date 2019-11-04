39dc948dd2cc740e05b896d9ea80d1d3ec477285
wukrit <sukritwalia@gmail.com>
2019-11-04 18:27:38 -0500

    create user is can now also log users in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
 
     def create
-        user = User.create(user_params)
-        if user.valid?
-            payload = { user_id: user.id }
-            token = JWT.encode(payload, secret, 'HS256')
-            render json: {user: user, token: token}
+        if (User.find_by(email: user_params[:email]))
+            user = User.find_by(email: user_params[:email])
+            if user && user.authenticate(user_params[:password])
+                payload = { user_id: user.id }
+                token = JWT.encode(payload, secret, 'HS256')
+                render json: {
+                    user: {
+                        id: user.id,
+                        name: user.name,
+                        email: user.email,
+                        username: user.username
+                    },
+                    token: token
+                }
+            end
         else
-            render json: {errors: user.errors.full_messages}
+            user = User.create(user_params)
+            if user.valid?
+                payload = { user_id: user.id }
+                token = JWT.encode(payload, secret, 'HS256')
+                render json: {user: user, token: token}
+            else
+                render json: {errors: user.errors.full_messages}
+            end
         end
     end
 
