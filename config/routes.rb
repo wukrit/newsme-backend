b72d1515a63050cfa23955d3afc5a76110ffbc45
wukrit <sukritwalia@gmail.com>
2019-11-05 11:20:33 -0500

    created delete account action

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :subscriptions, only: [:index, :create, :destroy]
   resources :filters, only: [:index, :create, :destroy]
   resources :articles, only: [:index, :show]
-  resources :users, only: [:create, :destroy]
+  resources :users, only: [:create]
 
   post '/login', to: 'login#login'
   get '/login', to: 'login#persist'
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   get '/users/feed', to: 'users#feed'
   get '/users/subscriptions', to: 'users#subscriptions'
   patch '/users/edit', to: 'users#edit'
+  get '/users/delete', to: 'users#delete_account'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
