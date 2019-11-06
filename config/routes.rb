9be3843cd28fe3a5f07144aa48e4ff462b6974f0
Wukrit <sukritwalia@gmail.com>
2019-11-06 12:58:22 -0500

    updated routes, added jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 <<<<<<< HEAD
+<<<<<<< HEAD
   resources :subscriptions, only: %i[index show create destroy]
   resources :filters, only: %i[index show create destroy]
   resources :sources, only: %i[index show]
@@ -14,5 +15,16 @@ Rails.application.routes.draw do
   resources :topics
   resources :users
 >>>>>>> daa9ce2... Rebuilt migrations to account for reserved words
+=======
+  resources :news_sources, only: [:index, :show]
+  resources :subscriptions, only: [:index, :create, :destroy]
+  resources :filters, only: [:index, :create, :destroy]
+  resources :articles, only: [:index, :show]
+  resources :topics, only: [:index, :show]
+  resources :users, only: [:show, :create, :update, :destroy]
+
+  post '/login', to: 'login#login'
+  get '/login', to: 'login#persist'
+>>>>>>> cae14c1... updated routes, added jwt
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
