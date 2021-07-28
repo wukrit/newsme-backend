f8ac706b61e95350669a6a632745705a972ffac9
Sukrit Walia <sukritwalia@gmail.com>
2021-07-28 16:53:33 -0400

    Create tests for user newsletter methods.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,9 +7,24 @@ class UserTest < ActiveSupport::TestCase
     @article = create(:article, topic: sub.topic)
   end
 
-  test '#serve' do
+  test '#serve generates a user news feed' do
     user_feed = @user.serve
     expected_feed = { "Technology" => [@article] }
     assert_equal expected_feed, user_feed
+  end
+
+  test '#send_newsletter sends an email' do
+    @user.send_newsletter
+
+    assert_emails 1
+  end
+
+  test '.send_newsletters sends an email for each user' do
+    new_user = create(:user)
+    new_sub = create(:subscription, user: new_user, topic: @article.topic)
+
+    assert_emails 2 do
+      User.send_newsletters
+    end
   end
 end
