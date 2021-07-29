be8ab19ee9aabdb7d9a4458fefa323c168071f86
Sukrit Walia <sukritwalia@gmail.com>
2021-07-28 21:13:28 -0400

    Add unit test for topic model.

--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -4,4 +4,16 @@ class TopicTest < ActiveSupport::TestCase
   setup do
     @topic = create(:topic)
   end
+
+  test ".list returns an array of topics" do
+    assert_instance_of Array, Topic.list
+  end
+
+  test "#serve returns an array of articles of the current topic from the current day" do
+    create(:article, topic: @topic)
+    create(:article, topic: @topic, date: "not_a_date")
+    create(:article)
+
+    assert_equal 1, @topic.serve.count
+  end
 end
